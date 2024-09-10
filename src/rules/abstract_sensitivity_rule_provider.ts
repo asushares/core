de4c3b5a53a7c71a60eabbab1386382cb1257aac
Preston Lee <preston.lee@prestonlee.com>
2024-09-10 10:37:57 -0700

    Minor formatting change for publication.

--- src/rules/abstract_sensitivity_rule_provider.ts
+++ src/rules/abstract_sensitivity_rule_provider.ts
@@ -61,22 +61,19 @@ export abstract class AbstractSensitivityRuleProvider {
 
 
     applicableRulesFor(codings: Coding[], allRules: Rule[], threshold: number): Rule[] {
-        // let codeList = codings.map(c => { return c.code; });
         let rules = allRules.filter(rule => {
-            // console.log('Considering rule: ' + rule.id);
-            // return rule.allExpendedCodes().some(coding => {
             return rule.allCodeObjects().some(coding => {
-                // console.log("\tRule coding: " + coding.system + ' ' + coding.code);
                 let found = false;
                 for (let i = 0; i < codings.length; i++) {
-                    // console.log(coding);
                     if (coding.system == codings[i].system && coding.code == codings[i].code) {
                         if (coding.confidence >= threshold) {
                             found = true;
                             console.log("Rule sensitivity match on: " + coding.system + ' ' + coding.code);
                             break;
                         } else {
-                            console.log(`Rule sensitivity match SKIPPED due to low confidence (confidence ${coding.confidence} < configured threshold ${threshold}) for: ` + coding.system + ' ' + coding.code);
+                            console.log(`Rule sensitivity match SKIPPED due to low confidence ` +
+                                `(confidence ${coding.confidence} < configured threshold ${threshold}) for: ` +
+                                coding.system + ' ' + coding.code);
                         }
                     }
                 }
